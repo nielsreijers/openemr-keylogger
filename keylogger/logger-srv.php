7e19b410e8956331a97744ad79df451f3a7c0dec
Niels Reijers <nielsreijers@gmail.com>
2020-08-12 17:40:43 +0800

    Log to MySQL table

--- keylogger/logger-srv.php
+++ keylogger/logger-srv.php
@@ -1,8 +1,26 @@
 <?php
+
+require_once("../interface/globals.php");
+require_once("../library/sql.inc");
+
 $entityBody = file_get_contents('php://input');
 if (!empty($entityBody)) {
-  $file = fopen('presses.txt', 'a+');
-  fwrite($file, $entityBody . PHP_EOL);
-  fclose($file);
+    // log to file
+    $file = fopen('presses.txt', 'a+');
+    fwrite($file, $entityBody . PHP_EOL);
+    fclose($file);
+
+    // log to sql
+    $decoded = json_decode($entityBody);
+    $user = $decoded->user;
+    foreach ($decoded->events as $event) {
+        if (property_exists($event, 'data')) {
+            $query = "INSERT INTO aa_keylogger_trace ( user, time, type, data ) VALUES ( '{$user}', {$event->time}, '{$event->type}', '{$event->data}' )";
+        } else {
+            $query = "INSERT INTO aa_keylogger_trace ( user, time, type, data ) VALUES ( '{$user}', {$event->time}, '{$event->type}', '' )";            
+        }
+        sqlInsert($query);
+    }
 }
 ?>
+
---
+++ keylogger/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS aa_keylogger_trace (
+  id          bigint(20)   NOT NULL auto_increment,
+  user        varchar(255),
+  time        bigint,
+  type        varchar(32),
+  data        varchar(255),
+ PRIMARY KEY (id)
+) ENGINE=InnoDB;
