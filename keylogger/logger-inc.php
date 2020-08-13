5909c1f0f3c89a9c13f3edd228d7b9b6c30cff92
Lee Hsun <speachlesslee@gmail.com>
2020-08-13 13:06:03 +0800

    Fix url paths for keylogger

--- keylogger/logger-inc.php
+++ keylogger/logger-inc.php
@@ -1,4 +1,4 @@
 <script>
     var vliegtuig_username = "<?php echo $_SESSION["authUser"]?>";
 </script>
-<script src="/openemr/keylogger/logger.js"></script>
+<script src="<?php echo $web_root;?>/keylogger/logger.js"></script>
